6396ee39b51d0455dbe73a9433aa59701c96cf80
Sam Zun <zunsamuel97@gmail.com>
2023-01-30 22:32:13 -0600

    created client and server

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <div id="app">
+        <h1>{{ response }}</h1>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script >
+import axios from "axios"
+
+export default {
+  data(){
+    return {
+      response: ""
+    }
+  },
+  mounted() {
+    this.getRequest()
+  },
+  methods: {
+    getRequest: async function () {
+      const response = await axios.get("http://127.0.0.1:5000/")
+      console.log("Response")
+      console.log(response)
+      this.response = response.data.test
+    }
+  }
+}
+</script>
